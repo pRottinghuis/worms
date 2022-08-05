32cdd212f07b38545a45aebd414c1223ad12d009
cfrishausen <p.rttnghs@outlook.com>
2022-08-05 12:34:17 -0700

    Project cleanup.

--- src/main/java/com/cfishausen/worms/event/WEventBusEvents.java
+++ src/main/java/com/cfishausen/worms/event/WEventBusEvents.java
@@ -4,9 +4,16 @@ import com.cfishausen.worms.Worms;
 import com.cfishausen.worms.entity.WEntityTypes;
 import com.cfishausen.worms.entity.animal.custom.WormEntity;
 import com.cfishausen.worms.entity.client.WormBobberRenderer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -16,10 +23,5 @@ public class WEventBusEvents {
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
         event.put(WEntityTypes.WORM.get(), WormEntity.setAttributes());
-    }
-
-    @SubscribeEvent
-    public static void RegisterEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        //event.registerEntityRenderer((EntityType)WEntityTypes.WORM_FISHING_BOBBER.get(), WormBobberRenderer::new);
     }
 }
